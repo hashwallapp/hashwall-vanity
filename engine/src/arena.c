ab07b8ed13c211d6de19eb250cdbca4be159fe51
slpng <mottomottohora@gmail.com>
2026-05-18 17:51:05 +0300

    arena: add support for custom memory alignment

--- engine/src/arena.c
+++ engine/src/arena.c
@@ -10,12 +10,17 @@ void arena_init(Arena *arena, void *memory, size_t capacity) {
     arena->used = 0;
 }
 
-#define arena_push_type(arena, type) (type *)arena_push_type_(arena, sizeof(type))
-#define arena_push_array(arena, count, type) (type *)arena_push_type_(arena, (count) * sizeof(type))
-void *arena_push_type_(Arena *arena, size_t size) {
-    assert((arena->used + size) <= arena->capacity && "ERROR: not enough memory to push to arena");
+#define arena_push_type(arena, type, alignment) (type *)arena_push_type_(arena, sizeof(type), alignment)
+#define arena_push_array(arena, count, type, alignment) (type *)arena_push_type_(arena, (count) * sizeof(type), alignment)
 
-    void *result = (uint8_t *)arena->memory + arena->used;
-    arena->used += size;
+void *arena_push_type_(Arena *arena, size_t size, size_t alignment) {
+    size_t padding = alignment - ((arena->used + size) % alignment);
+
+    assert((arena->used + size + padding) <= arena->capacity && "ERROR: not enough memory to push to arena");
+
+    void *result = (uint8_t *)arena->memory + arena->used + padding;
+
+    arena->used += size + padding;
+
     return result;
 }
